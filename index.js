b6fd4bee9349f7433443b991104555dfb4cdeb30
NoorSkeirek <120521132+NoorSkeirek@users.noreply.github.com>
2022-12-14 12:43:26 +0300

    Add files via upload
    install

---
+++ index.js
@@ -0,0 +1,330 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffaead"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 4,
+                "color": "#ff6c6c"
+            },
+            "polygon": {
+                "nb_sides": 10
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 1,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+// document.body.appendChild(stats.domElement);
+
+count_particles = document.querySelector('.js-count-particles');
+update = function () {
+    stats.begin();
+    stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+};
+requestAnimationFrame(update);;
+// ***********
+
+/***************** */
+
+let slideIndex = 0;
+showSlides();
+
+// Next-previous control
+function nextSlide() {
+    slideIndex++;
+    showSlides();
+    timer = _timer; // reset timer
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlides();
+    timer = _timer;
+}
+
+// Thumbnail image controlls
+function currentSlide(n) {
+    slideIndex = n - 1;
+    showSlides();
+    timer = _timer;
+}
+
+function showSlides() {
+    let slides = document.querySelectorAll(".mySlides");
+    let dots = document.querySelectorAll(".dots");
+
+    if (slideIndex > slides.length - 1) slideIndex = 0;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+
+    // hide all slides
+    slides.forEach((slide) => {
+        slide.style.display = "none";
+    });
+
+    // show one slide base on index number
+    slides[slideIndex].style.display = "block";
+
+    dots.forEach((dot) => {
+        dot.classList.remove("active");
+    });
+
+    dots[slideIndex].classList.add("active");
+}
+
+// autoplay slides --------
+let timer = 7; // sec
+const _timer = timer;
+
+// this function runs every 1 second
+setInterval(() => {
+    timer--;
+
+    if (timer < 1) {
+        nextSlide();
+        timer = _timer; // reset timer
+    }
+}, 1000); // 1sec
+
+/************************* */
+
+particlesJS("particles-js2", {
+    "particles": {
+        "number": {
+            "value": 10,
+            "density": {
+                "enable": true,
+                "value_area": 500
+            }
+        },
+        "color": {
+            "value": "#fff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 80,
+                "color": "#808080"
+            },
+            "polygon": {
+                "nb_sides": 10
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 200,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 1,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.8,
+            "width": 5
+        },
+        "move": {
+            "enable": true,
+            "speed": 4,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+// document.body.appendChild(stats.domElement);
+
+count_particles = document.querySelector('.js-count-particles');
+update = function () {
+    stats.begin();
+    stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+/************************************ */
+// $('.testimonial-slider').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     arrows: false,
+//     adaptiveHeight: true
+// });
